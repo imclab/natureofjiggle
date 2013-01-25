0970699b6ea2245ac90a0a2feec4f2af83d2c9d4
George Michael Brower <george.brower@gmail.com>
2013-01-24 20:43:59 -0500

    ipad

--- js/main.js
+++ js/main.js
@@ -1,50 +1,43 @@
-
+var BASE_SCALE = 2;
 var STROKE_WEIGHT = 10;
-
+var WINDOW_SCALE = 1;
+var MIN_STRENGTH = 0.1;
+var MAX_STRENGTH = 3;
+var MAX_DAMPING = 1;
+var MIN_DAMPING = 0.01;
+var PADDING = 100;
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
 var elastic = new Elastic();
 var elastic2 = new Elastic();
 var domReadout = document.getElementById('readout');
 var domTest = document.getElementById('test');
+var domEssay = document.getElementById('essay');
 var framesSinceClick = 0;
 var clicked = false;
 var TWO_PI = Math.PI*2;
 var values = [];
 var path;
-var params = {
-  disp: -1,
-  STROKE_WEIGHT: 2,
-  STROKE_COLOR: '#135478',
-  DOT_COLOR: '#f0194c',
-  CIRCLE_COLOR: '#f2f0e4'
-};
+var x = 0, y = 0, px, py;
 
 
-// var gui = new dat.GUI();
-// gui.addColor(params, 'STROKE_COLOR');
-// gui.add(params, 'STROKE_WEIGHT', 0, 10);
-// gui.addColor(params, 'DOT_COLOR');
-// gui.addColor(params, 'CIRCLE_COLOR');
-
-
-$(document.body).append(canvas);
+$(document.body).prepend(canvas);
 
 paper.setup(canvas);
 
 if (Modernizr.touch) {
-  document.body.style.height = '90000px';
-  window.scrollTo(0, 1);
-
-  window.addEventListener('touchstart', setSpringParams, true);
+  // document.body.style.height = '90000px';
+  // window.scrollTo(-1, 0);
+  window.addEventListener('touchstart', touchStart, true);
   window.addEventListener('touchmove', setSpringParams, true);
   window.addEventListener('touchend', previewJiggle, true);
 } else { 
-  window.addEventListener('resize', onWindowResize, false);
   window.addEventListener('mousemove', setSpringParams, true);
   window.addEventListener('mousedown', previewJiggle, false);  
 }
 
+window.addEventListener('resize', onWindowResize, false);
+window.addEventListener('orientationchange', onWindowResize, false);
 
 elastic2.dest = 1;
 elastic2.value = 1;
@@ -55,40 +48,77 @@ function previewJiggle() {
   elastic2.value = 0;
 }
 
+function touchStart(e) {
+
+  px = event.touches[0].pageX;
+  py = event.touches[0].pageY;
+
+}
+
 function setSpringParams(e) {
 
   e.preventDefault();
 
-  var x = Modernizr.touch ? event.touches[0].pageX : e.clientX;
-  var y = Modernizr.touch ? event.touches[0].pageY : e.clientY;
+  if (Modernizr.touch) {
+    var cx = event.touches[0].pageX;
+    var cy = event.touches[0].pageY;
+    x += cx - px;
+    y += cy - py;
+    x = constrain(x, 0, window.innerWidth);
+    y = constrain(y, 0, window.innerHeight);
+    px = cx;
+    py = cy;
+  } else { 
+    x = e.clientX;
+    y = e.clientY;
+  }
 
-
-  elastic2.damping = elastic.damping = Math.pow(rnd.map(y, 0, window.innerHeight, 1, 0), 0.33);
-  elastic2.strength = elastic.strength = rnd.map(x, 0, window.innerWidth, 3, 0);
+  elastic2.damping = elastic.damping = yToDamping(y);
+  elastic2.strength = elastic.strength = xToStrength(x);
   plot();
+
 }
+
+
 
 function onWindowResize() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+
+  setScale(domEssay, 1);
+  setScale(domEssay.firstElementChild, 1);
+
+  var essayRect = domEssay.getBoundingClientRect();
+  var w = essayRect.width + PADDING;
+  WINDOW_SCALE = Math.min(window.innerWidth, window.innerHeight) / w;
+
+  setScale(domEssay, WINDOW_SCALE/BASE_SCALE);
+  setScale(domEssay.firstElementChild, WINDOW_SCALE * BASE_SCALE);
+
+  domEssay.style.marginTop = -(w-PADDING)/2 - 6 + 'px'; // Because it sits on a line all weird.
+  domEssay.style.marginLeft = -(w-PADDING)/2 + 'px';
+
+  var ratio = url.boolean('usePixelRatio', true) ? window.devicePixelRatio || 1 : 1;
+
+  canvas.width = Math.ceil(ratio*window.innerWidth);
+  canvas.height = Math.ceil(ratio*window.innerHeight);
+
   plot();
 
 }
 
-onWindowResize();
+
 
 function plot() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   elastic.velocity = 0;
-  elastic.value = params.disp;
+  elastic.value = -1;
 
   if (path) path.remove();
   values.length = 0;
   path = new paper.Path();
   path.strokeCap = 'round';
-  path.strokeColor = params.STROKE_COLOR;
-  path.strokeWidth = Math.max(2, Math.min(canvas.width, canvas.height)/300);
-
+  path.strokeWidth = 1.5 * window.devicePixelRatio;
+  path.strokeColor = '#333';
 
   var steps = 100;
 
@@ -101,42 +131,43 @@ function plot() {
     var pos = getPos(i, steps, elastic.value);
 
     if (i == 0)
-      path.moveTo(new paper.Point(pos.x, pos.y));
+      path.moveTo(new paper.Point(pos.x, Math.floor(pos.y)));
     else
-      path.lineTo(new paper.Point(pos.x, pos.y));
+      path.lineTo(new paper.Point(pos.x, Math.floor(pos.y)));
 
     elastic.update();
 
   }
 
+
   path.smooth();
+  paper.view.draw();
+
 
 }
 
-plot();
 
 function loop() {
   
   requestAnimationFrame(loop);
   
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   elastic2.update();
   
-  framesSinceClick++;
+  // framesSinceClick++;
+  // ctx.fillStyle = params.CIRCLE_COLOR;
+  // ctx.globalAlpha = 0.95;
+  // ctx.save();
+  // ctx.translate(canvas.width/2, canvas.height/2);
+  // ctx.scale(elastic2.value, elastic2.value);
+  // circle(0, 0, Math.min(canvas.width, canvas.height)/3);
+  // ctx.restore();
+  // ctx.globalAlpha = 1;
 
-  paper.view.draw();
-
-  ctx.fillStyle = params.CIRCLE_COLOR;
-  ctx.globalAlpha = 0.95;
-  ctx.save();
-  ctx.translate(canvas.width/2, canvas.height/2);
-  ctx.scale(elastic2.value, elastic2.value);
-  circle(0, 0, Math.min(canvas.width, canvas.height)/3);
-  ctx.restore();
-  ctx.globalAlpha = 1;
+  setScale(domEssay, elastic2.value / BASE_SCALE );
 
 
+  // console.log(elastic2.value);
 
   // if (clicked && framesSinceClick < values.length) {
   //   var pos = getPos(framesSinceClick, values.length, values[framesSinceClick]);
@@ -146,7 +177,20 @@ function loop() {
 
 }
 
-loop();
+
+function constrain(v, min, max) {
+  if (v > max) return max;
+  if (v < min) return min;
+  return v;
+}
+
+function setScale(elem, s) {
+  elem.style.webkitTransform  = 'scale('+s+')';
+  elem.style.mozTransform     = 'scale('+s+')';
+  elem.style.msTransform      = 'scale('+s+')';
+  elem.style.oTransform       = 'scale('+s+')';
+  elem.style.transform        = 'scale('+s+')';
+}
 
 function circle(x, y, d) {
   ctx.beginPath();
@@ -155,10 +199,22 @@ function circle(x, y, d) {
 }
 
 function getPos(i, steps, value) {
-    var x = rnd.map(i, 0, steps-1, 0, canvas.width);
-    var y = rnd.map(value, elastic.dest, 1, 0, -canvas.height/2) + canvas.height/2;
-    return { 
-      x: x,
-      y: y
-    };
+  var x = rnd.map(i, 0, steps-1, 0, canvas.width);
+  var y = rnd.map(value, elastic.dest, 1, 0, -canvas.height/2) + canvas.height/2;
+  return { 
+    x: x,
+    y: y
+  };
 }
+
+function yToDamping(y) {
+  return Math.pow(rnd.map(y, 0, window.innerHeight, MAX_DAMPING, MIN_DAMPING), 2);
+}
+
+function xToStrength(x) {
+  return rnd.map(x, 0, window.innerWidth, MAX_STRENGTH, MIN_STRENGTH);
+}
+
+onWindowResize();
+plot();
+loop();
